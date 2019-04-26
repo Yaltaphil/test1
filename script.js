2bae83108031ae892d6fa6fee28155f14ab91d3e
Yaltaphil <yaltaphil@yandex.ru>
2019-04-26 15:29:47 +0300

    last version

--- script.js
+++ script.js
@@ -19,29 +19,29 @@ AppData.expenses.a1 = a2;
 AppData.expenses.a3 = a4;
 alert(AppData.budget / 30);
 
-// if (2 * 2 == 5) {
-//     console.log("Верно")
-// } else {
-//     console.log("Неверно")
-// }
+if (2 * 2 == 5) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
 
-// (2 == 2) ? console.log("Yes"): console.log("No");
+(2 == 2) ? console.log("Yes"): console.log("No");
 
-// let Numb = 5;
-// switch (true) {
-//     case Numb < 5:
-//         console.log("less");
-//         break;
-//     case Numb == 5:
-//         console.log("equal");
-//         break;
-//     case Numb > 5:
-//         console.log("bigger");
-//         break;
-//     default:
-//         console.log("ok");
-//         break;
-// }
+let Numb = NaN;
+switch (true) {
+    case Numb < 5:
+        console.log("less");
+        break;
+    case Numb == 5:
+        console.log("equal");
+        break;
+    case Numb > 5:
+        console.log("bigger");
+        break;
+    default:
+        console.log("ok");
+        break;
+}
 
 let num = 53;
 while (num < 55) {
